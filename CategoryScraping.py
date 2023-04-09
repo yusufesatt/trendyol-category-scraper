d3df043d1d42cb4c4f852f78b7f25a8778c6131d
Spyzah <xyusufesat@gmail.com>
2023-04-10 01:31:21 +0300

    Api version update

--- CategoryScraping.py
+++ CategoryScraping.py
@@ -2,47 +2,52 @@ from requests import get
 from json import loads
 import pandas as pd
 
-url = input("URL: ")
+try:
+    url = input("URL: ")
 
-print('Tüm ürünleri çekmek için 0 bas')
-Range = int(input("Kaç ürün çekmek istiyorsun: "))
+    print('Tüm ürünleri çekmek için 0 bas')
+    Range = int(input("Kaç ürün çekmek istiyorsun: "))
 
-pageCount= 1
-productList = []
 
-if ('q=') in url:
-    keywords = url.split('q=')[-1].split("&qt")[0]
-    dataUrl = f'https://public.trendyol.com/discovery-web-searchgw-service/v2/api/infinite-scroll/sr?q={keywords}&pi={pageCount}'
-else:
-    keyword = url.split("com/")[-1].split("?")[0]
-    dataUrl = f'https://public.trendyol.com/discovery-web-searchgw-service/v2/api/infinite-scroll/{keyword}?pi={pageCount}'
 
-while True:
-    if len(productList) >= Range and Range != 0 or pageCount > 208:
-        break
-    datas = loads(get(dataUrl).text)
-    productList.extend(datas["result"]["products"])
+    pageCount= 1
+    productList = []
 
-    if len(datas["result"]['products']) == 24:
-        pageCount += 1
+    if ('q=') in url:
+        keywords = url.split('q=')[-1].split("&qt")[0]
+        dataUrl = f'https://public.trendyol.com/discovery-web-searchgw-service/v2/api/infinite-scroll/sr?q={keywords}&pi={pageCount}'
     else:
-        break
+        keyword = url.split("com/")[-1].split("?")[0]
+        dataUrl = f'https://public.trendyol.com/discovery-web-searchgw-service/v2/api/infinite-scroll/{keyword}?pi={pageCount}'
 
-productList = productList[:Range] if Range > 0 else productList
+    while True:
+        if len(productList) >= Range and Range != 0 or pageCount > 208:
+            break
+        datas = loads(get(dataUrl).text)
+        productList.extend(datas["result"]["products"])
 
-productName = [name["name"] for name in productList]
-productLink = [link["url"] for link in productList]
-productImg = [img["images"] for img in productList]
-proudctPrice = [str(price["price"]["sellingPrice"]) + " TL" for price in productList]
+        if len(datas["result"]['products']) == 24:
+            pageCount += 1
+        else:
+            break
 
-urunListesi = []
+    productList = productList[:Range] if Range > 0 else productList
 
-for i in range(len(productName)):
-    urunListesi.append([productName[i], "https://trendyol.com"+productLink[i], "https://cdn.dsmcdn.com"+productImg[i][0], proudctPrice[i]])
+    productName = [name["name"] for name in productList]
+    productLink = [link["url"] for link in productList]
+    productImg = [img["images"] for img in productList]
+    proudctPrice = [str(price["price"]["sellingPrice"]) + " TL" for price in productList]
 
-df1 = pd.DataFrame(urunListesi,columns=(["Urun İsmi", "Urun Linki", "Urun Resmi", "Urun Fiyatı"]))
-df1.to_excel("urunler.xlsx", sheet_name="urun", index=False)
+    urunListesi = []
 
+    for i in range(len(productName)):
+        urunListesi.append([productName[i], "https://trendyol.com"+productLink[i], "https://cdn.dsmcdn.com"+productImg[i][0], proudctPrice[i]])
+
+    df1 = pd.DataFrame(urunListesi,columns=(["Urun İsmi", "Urun Linki", "Urun Resmi", "Urun Fiyatı"]))
+    df1.to_excel("urunler.xlsx", sheet_name="urun", index=False)
+    print("Basariyla excel'e aktarıldı :)")
+except:
+    print("Islem basarsiz oldu url'yi kontrol ediniz.")
 
 
 
